7aa215384aa8a242e6a3b08d7d086936ba526430
Zigmas <zambrazaitis@gmail.com>
2018-01-31 11:59:48 +0200

    uzduotis3.03

---
+++ src/com/company/Main.java
@@ -0,0 +1,21 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        // parasyti programa, kuri paprasytu vesti skaicius tol, kol bus ivestas skaicius 0.
+        // Pabaigoje turi buti atvaizduojama skaiciu suma
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Iveskite skaiciu: ");
+        int skaicius = sc.nextInt();
+        int suma = 0;
+        while (skaicius != 0) {
+            System.out.println("Iveskite skaiciu: ");
+            skaicius = sc.nextInt();
+            suma = suma + skaicius;
+        }
+        System.out.println("Ivestu skaiciu suma: " + suma);
+    }
+}
